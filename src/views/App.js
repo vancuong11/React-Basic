6443237911331911da88c2c2e59fa6c97e3ba239
vancuong11 <cuongg121101@gmail.com>
2023-03-22 10:42:09 +0700

    Components

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './Example/MyComponent';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
+                <MyComponent />
             </header>
         </div>
     );
