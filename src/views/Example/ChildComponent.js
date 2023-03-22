4d8a649827660546ccaa7a525a64e9e473ad7b68
vancuong11 <cuongg121101@gmail.com>
2023-03-22 14:45:06 +0700

    Su dung dieu kien voi React

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,44 +1,45 @@
 import React from 'react';
 
-// class ChildComponent extends React.Component {
-//     // key: value
-//     state = {
-//         firstName: '',
-//         lastName: '',
-//     };
+class ChildComponent extends React.Component {
+    // key: value
+    state = {
+        showJobs: false,
+    };
 
-//     render() {
-//         let { arrJobs } = this.props;
-//         return (
-//             <div className="job-list">
-//                 {arrJobs.map((item, index) => {
-//                     return (
-//                         <div key={item.id}>
-//                             {item.title} - {item.salary}
-//                         </div>
-//                     );
-//                 })}
-//             </div>
-//         );
-//     }
-// }
+    handleShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs,
+        });
+    };
 
-const ChildComponent = (props) => {
-    let { arrJobs } = props;
-
-    return (
-        <>
-            <div className="job-lists">
-                {arrJobs.map((item, index) => {
-                    return (
-                        <div key={item.id}>
-                            {item.title} - {item.salary}
+    render() {
+        let { arrJobs } = this.props;
+        let { showJobs } = this.state;
+        return (
+            <>
+                {showJobs === false ? (
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>Show</button>
+                    </div>
+                ) : (
+                    <>
+                        <div className="job-list">
+                            {arrJobs.map((item, index) => {
+                                return (
+                                    <div key={item.id}>
+                                        {item.title} - {item.salary} &nbsp;$
+                                    </div>
+                                );
+                            })}
                         </div>
-                    );
-                })}
-            </div>
-        </>
-    );
-};
+                        <div>
+                            <button onClick={() => this.handleShowHide()}>Hide</button>
+                        </div>
+                    </>
+                )}
+            </>
+        );
+    }
+}
 
 export default ChildComponent;
