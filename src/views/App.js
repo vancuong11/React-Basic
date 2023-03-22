aa62a36410979269577e055ff19e4776e341377b
vancuong11 <cuongg121101@gmail.com>
2023-03-22 15:32:07 +0700

    ToDo Apps

--- src/views/App.js
+++ src/views/App.js
@@ -1,17 +1,34 @@
 import logo from './logo.svg';
 import './App.scss';
 import MyComponent from './Example/MyComponent';
+import ListTodo from './Todos/ListTodo';
+
+// ----------------------------------------------------------------
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <MyComponent />
+                <p>Simple TODO Apps with React.js</p>
+                {/* <MyComponent /> */}
+                <ListTodo />
             </header>
+
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     );
 }
