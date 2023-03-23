5ae53f0070da5a0d973bb835723fb720897fce57
vancuong11 <cuongg121101@gmail.com>
2023-03-23 14:29:56 +0700

    Redirect voi React

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 class Home extends React.Component {
+    // life cycle React
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/todo');
+        }, 3000);
+    }
     render() {
+        console.log('check props: ', this.props);
         return <div>Hello world from Homepage with Eric & Hoi Dan IT</div>;
     }
 }
 
-export default Home;
+export default withRouter(Home);
