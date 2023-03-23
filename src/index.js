127a8aa24cb0a31cd48a749ba9173e4caaa148f6
vancuong11 <cuongg121101@gmail.com>
2023-03-23 14:21:51 +0700

    Links & NavLinks trong React

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './styles/global.scss';
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// -------------- Version 18 --------------------------------------------------
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+// );
+
+// -------------- Version 17 --------------------------------------------------
+ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './Nav.scss';
 
+import { Link, NavLink } from 'react-router-dom';
+
 class Nav extends React.Component {
     render() {
         return (
             <div className="topnav">
-                <a className="active" href="/">
+                <NavLink to="/" activeClassName="active" exact={true}>
                     Home
-                </a>
-                <a href="/todo">Todos</a>
-                <a href="/about">About</a>
+                </NavLink>
+                <NavLink to="/todo" activeClassName="active">
+                    Todos
+                </NavLink>
+                <NavLink to="/about" activeClassName="active">
+                    About
+                </NavLink>
+
+                {/* <Link className="active" to="/">
+                    Home
+                </Link>
+                <Link to="/todo">Todos</Link>
+                <Link to="/about">About</Link> */}
             </div>
         );
     }
