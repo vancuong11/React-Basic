e67fe79ff004cab48419a38baaa5dddf28d16f68
vancuong11 <cuongg121101@gmail.com>
2023-03-25 14:30:22 +0700

    Higher Order Component

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import Color from '../HOC/color';
 
 class Home extends React.Component {
     // life cycle React
-    componentDidMount() {
-        setTimeout(() => {
-            this.props.history.push('/todo');
-        }, 3000);
-    }
+    // componentDidMount() {
+    //     setTimeout(() => {
+    //         this.props.history.push('/todo');
+    //     }, 3000);
+    // }
     render() {
         console.log('check props: ', this.props);
         return <div>Hello world from Homepage with Eric & Hoi Dan IT</div>;
     }
 }
 
-export default withRouter(Home);
+// export default withRouter(Home);
+export default Color(Home);
